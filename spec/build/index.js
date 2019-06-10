feed763dbd8870739957bb584b78d342856c2ed7
feenr <Ryan.Feeney@pega.com>
2019-06-10 08:16:54 -0400

    Remove build files from index

--- spec/build/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const client_1 = require("./client");
-// @ts-ignore
-window.Tracer = client_1.Client;
-//# sourceMappingURL=index.js.map
