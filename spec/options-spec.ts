cf5b9e8a769b183325b6d3d70455f7f3122968b5
Nanglo <Tenzin.Nanglo@pega.com>
2019-06-14 10:27:50 -0400

    making changes adds a code cov report for branch 18

--- spec/options-spec.ts
+++ spec/options-spec.ts
@@ -9,4 +9,16 @@ describe("Options", function() {
     it("can be initialised", function(){
         expect(typeof options).toEqual("object");
     });
+
+    it("Clear Function returns an empty array", function () {
+
+        options.applyDefaults();
+        options.clear();
+
+        let optionsString = options.getOption("nOptTraceClassLoad");
+
+        expect(optionsString).toEqual(undefined);
+
+    });
+
 });
